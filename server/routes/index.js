9b70e951be64fb7a9bb4c645bcfb5afccd3f5b10
LuisAlavez183 <x089307@gmail.com>
2021-09-25 04:55:43 -0500

    Se crea la carpeta de Server

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HJFGJJDSFSDFF' });
+  res.render('index', { title: 'HJFGJJDSFSDFF', firstname:'luis', hola:'garcia'});
 });
 
 module.exports = router;
