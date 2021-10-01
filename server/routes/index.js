4099b22b2e11dd33dd99c8a5efc054eb1ae4eb3c
LuisAlavez183 <x089307@gmail.com>
2021-10-01 07:47:22 -0500

    instalado webpack

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HJFGJJDSFSDFF', firstname:'luis', hola:'garcia'});
+  res.render('index', { title: 'HJFGJJDSFSDFF', firstname:'pedro', hola:'garcia'});
 });
 
 module.exports = router;
