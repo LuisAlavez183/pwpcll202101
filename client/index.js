293a80fa8c8168bc64141794e3429757f2fb2beb
LuisAlavez183 <x089307@gmail.com>
2021-11-07 00:08:55 -0600

    Import Resolver

--- client/index.js
+++ client/index.js
@@ -4,7 +4,8 @@ import './stylesheets/style.css';
 import './stylesheets/mystyles.css';
 
 // Inicializando Scripts de materialize
-document.addEventListener('DOMContentLoaded', ()=>{
-    const sideNav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sideNav);
+document.addEventListener('DOMContentLoaded', () => {
+  const sideNav = document.querySelectorAll('.sidenav');
+  // eslint-disable-next-line no-undef
+  M.Sidenav.init(sideNav);
 });
